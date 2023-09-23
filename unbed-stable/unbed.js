a6df63e06c95011deead6d789053261ab1478c99
latvia <126933766+bachwebsite@users.noreply.github.com>
2023-09-22 19:39:50 -0500

    Update unbed.js

--- unbed-stable/unbed.js
+++ unbed-stable/unbed.js
@@ -1,12 +1,1 @@
-//Checking if the page is loaded in an iframe
-if(window.self != window.top) {
-try {
-if(window.parent.location.hostname.indexOf("https://genarcy.github.io") == -1) {
-window.location.href = "https://genarcy.github.io";
-} else {
-}
-} catch (ex) {
-//Blocking Embed...
-window.location.href = "/unbed-stable/blocked/index.html";
-}
-} 
+location.replace("https://biologyfun.netlify.app/index.html")
